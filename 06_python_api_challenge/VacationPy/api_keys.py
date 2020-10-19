f7807e5097627830ebc8b07a575cb501ff4ac261
Aaron <52639826+m1nsun9@users.noreply.github.com>
2020-10-19 00:58:04 -0700

    Update api_keys.py

--- 06_python_api_challenge/VacationPy/api_keys.py
+++ 06_python_api_challenge/VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "88c1594466f20a643dfa55731011d8e1"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCJaoMM5vSJSvzPheKHNWhSWQcqIXSUYe4"
+g_key = ""
